e79e307469a6332a3db4ba750ac8356c56a822a2
Gor Stepanyan <gor.stepanyan027@gmail.com>
2020-06-01 04:07:50 +0400

    Initial Commit

---
+++ minix_source/sync.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" # set current working directory to the directory of the script
+
+rsync -uav --exclude=".git" ./usr/include/ minix:/usr/include/
+rsync -uav --exclude=".git" ./usr/src/ minix:/usr/src/
